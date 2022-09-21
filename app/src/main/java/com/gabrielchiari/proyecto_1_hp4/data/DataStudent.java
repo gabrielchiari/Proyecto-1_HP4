26ed2e4288337eeb39c7569409a98459c8c0b4b7
Felipe Loo <felipeloozhang@gmail.com>
2022-09-21 12:22:32 -0500

    DataClass fixed

--- app/src/main/java/com/gabrielchiari/proyecto_1_hp4/data/DataStudent.java
+++ app/src/main/java/com/gabrielchiari/proyecto_1_hp4/data/DataStudent.java
@@ -1,15 +1,27 @@
 package com.gabrielchiari.proyecto_1_hp4.data;
 
 public class DataStudent {
-    private String cedula;
+    private String cedula, nombre, apellido, genero;
     private boolean voted = false;
 
-    public DataStudent(String ced) {
-        this.cedula = ced;}
+    public DataStudent(String ced, String nom, String ape, String gene) {
+        this.cedula = ced;
+        this.nombre = nom;
+        this.apellido = ape;
+        this.genero = gene;}
 
 
     public String getCedula() {return cedula;}
     public void setCedula(String ced) {this.cedula = ced;}
+
+    public String getNombre() {return nombre;}
+    public void setNombre(String nom) {this.nombre = nom;}
+
+    public String getApellido() {return apellido;}
+    public void setApellido(String ape) {this.apellido = ape;}
+
+    public String getGenero() {return genero;}
+    public void setGenero(String gene) {this.genero = gene;}
 
     public boolean getVoted() {return voted;}
     public void setVoted(boolean votado) {this.voted = votado;}
